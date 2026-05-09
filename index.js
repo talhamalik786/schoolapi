1ee8c8b9f84126b4d53db906b1d2f522d3c38c87
Talha Malik <talhamalik6336@gmail.com>
2026-05-10 01:27:13 +0530

    fixed railway port issue

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get("/",(req,res)=>{
 
 app.use("/schoolapi",schoolRoutes)
 
-const PORT = 3000;
+const PORT =  process.env.PORT||3000;
 
 app.listen(PORT, () => {   
       console.log(`Server running at port ${PORT}`);
